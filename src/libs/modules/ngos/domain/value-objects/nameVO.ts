3cf9ea33de9a1543c89b30cb315ce502d01c7669
CaioCPS <caio_cesar_ps@hotmail.com>
2023-04-24 17:30:32 -0300

    fix: 🐛 remove the default from NameVO

--- src/libs/modules/ngos/domain/value-objects/nameVO.ts
+++ src/libs/modules/ngos/domain/value-objects/nameVO.ts
@@ -1,4 +1,4 @@
-export default class NameVO {
+export class NameVO {
   constructor(public firstName: string, public lastName: string) {
     if (!firstName) throw new Error('First name is required');
     if (!lastName) throw new Error('Last name is required');
